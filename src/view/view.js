303711c130d0cdf6b3ba4b3e611ebb92a5faf608
Chris.c <cjs120dd@gmail.com>
2022-08-19 21:44:30 +0900

    refactor: 함수 이름 변경

--- src/view/view.js
+++ src/view/view.js
@@ -3,42 +3,69 @@ import { SELECTOR } from '../constans/constans.js';
 import { TEMPLATES } from './templates.js';
 
 export class View {
-    constructor() {
-        this.init();
-    }
-    
-    init() {
-        this.renderHeader();
-    }
-    
-    renderHeader() {
-        const $header = document.createElement('header');
-        $header.innerHTML = TEMPLATES.HEADER;
-        $(SELECTOR.APP).appendChild($header);
-        this.registerTabHandlers();
-    }
-    
-    registerTabHandlers() {
-        this.productTabButtonHandler();
-        this.machineTabButtonHandler();
-        this.purchaseTabButtonHandler();
-    }
-    
-    productTabButtonHandler() {
-        $(SELECTOR.PRODUCT_MENU).addEventListener('click' , () => {
-            console.log(11);
-        });
-    }
-    
-    machineTabButtonHandler() {
-        $(SELECTOR.COIN_MENU).addEventListener('click' , () => {
-            console.log(22);
-        });
-    }
-    
-    purchaseTabButtonHandler() {
-        $(SELECTOR.PURCHASE_MENU).addEventListener('click' , () => {
-            console.log(33);
-        });
-    }
+	constructor() {
+		this.init();
+	}
+
+	init() {
+		this.renderHeader();
+		this.createContentsArea();
+	}
+
+	renderHeader() {
+		const $header = document.createElement('header');
+		$header.innerHTML = TEMPLATES.HEADER;
+		$(SELECTOR.APP).appendChild($header);
+		this.registerTabClicked();
+	}
+
+	createContentsArea() {
+		const $contentsArea = document.createElement('div');
+		$contentsArea.setAttribute('id', 'contents-area');
+		$(SELECTOR.APP).appendChild($contentsArea);
+	}
+
+	registerTabClicked() {
+		this.onProductTabButtonClicked();
+		this.onMachineTabClicked();
+		this.onPurchaseTabButtonClicked();
+	}
+
+	onProductTabButtonClicked() {
+		$(SELECTOR.PRODUCT_MENU).addEventListener('click', () => {
+			this.renderProductTab();
+		});
+	}
+
+	onMachineTabClicked() {
+		$(SELECTOR.COIN_MENU).addEventListener('click', () => {
+			this.renderMachineTab();
+		});
+	}
+
+	onPurchaseTabButtonClicked() {
+		$(SELECTOR.PURCHASE_MENU).addEventListener('click', () => {
+			console.log(33);
+		});
+	}
+
+	renderProductTab() {
+		const $productTab = document.createElement('div');
+		$productTab.innerHTML = TEMPLATES.PRODUCT_MENU;
+		this.clearContentsArea();
+		$(SELECTOR.CONTENTS_AREA).appendChild($productTab);
+	}
+
+	renderMachineTab() {
+		const $machineTab = document.createElement('div');
+		$machineTab.innerHTML = TEMPLATES.MACHINE_MENU;
+		this.clearContentsArea();
+		$(SELECTOR.CONTENTS_AREA).appendChild($machineTab);
+	}
+
+	clearContentsArea() {
+		if ($(SELECTOR.CONTENTS_AREA).hasChildNodes()) {
+			$(SELECTOR.CONTENTS_AREA).firstChild.remove();
+		}
+	}
 }
