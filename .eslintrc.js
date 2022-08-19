19d276b42e9a0678a47df0fd6e7674865f732f5d
Chris.c <cjs120dd@gmail.com>
2022-08-19 15:47:09 +0900

    chore: eslint 설정 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,17 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        node: true,
-    },
-    extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
-    plugins: ['prettier', 'mocha'],
-    rules: {
-        'import/prefer-default-export': 'off',
-        'import/extensions': ['off'],
-        "class-methods-use-this": "off",
-        "no-alert": "off",
-        "array-callback-return": "off",
-    }
-}
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
+	plugins: ['prettier', 'mocha'],
+	rules: {
+		'import/prefer-default-export': 'off',
+		'import/extensions': ['off'],
+		'class-methods-use-this': 'off',
+		'no-alert': 'off',
+		'no-undef': 'off',
+		'array-callback-return': 'off',
+	},
+};
