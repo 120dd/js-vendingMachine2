3f57cf537f837a9facbd680745930bede6c4130b
Chris.c <cjs120dd@gmail.com>
2022-08-20 15:51:42 +0900

    refactor:중복되는 함수 단일화

--- src/view/view.js
+++ src/view/view.js
@@ -4,7 +4,7 @@ import { SELECTOR, TEMPLATES } from './viewConstans.js';
 export class View {
 	constructor() {
 		this.$header = this.createElement('header', TEMPLATES.HEADER);
-		this.$contentsArea = this.createElement('div');
+		this.$contentsArea = this.createElement('div', '');
 		this.$productTab = this.createElement('div', TEMPLATES.PRODUCT_MENU);
 		this.$machineTab = this.createElement('div', TEMPLATES.MACHINE_MENU);
 		this.$purchaseTab = this.createElement('div', TEMPLATES.PURCHASE_MENU);
@@ -32,35 +32,25 @@ export class View {
 
 	registerProductTabButtonClicked() {
 		$(SELECTOR.PRODUCT_MENU).addEventListener('click', () => {
-			this.renderProductTab();
+			this.renderTab(this.$productTab);
 		});
 	}
 
 	registerMachineTabClicked() {
 		$(SELECTOR.COIN_MENU).addEventListener('click', () => {
-			this.renderMachineTab();
+			this.renderTab(this.$machineTab);
 		});
 	}
 
 	registerPurchaseTabButtonClicked() {
 		$(SELECTOR.PURCHASE_MENU).addEventListener('click', () => {
-			this.renderPurchaseTab();
+			this.renderTab(this.$purchaseTab);
 		});
 	}
 
-	renderProductTab() {
+	renderTab(tab) {
 		this.clearContentsArea();
-		$(SELECTOR.CONTENTS_AREA).appendChild(this.$productTab);
-	}
-
-	renderMachineTab() {
-		this.clearContentsArea();
-		$(SELECTOR.CONTENTS_AREA).appendChild(this.$machineTab);
-	}
-
-	renderPurchaseTab() {
-		this.clearContentsArea();
-		$(SELECTOR.CONTENTS_AREA).appendChild(this.$purchaseTab);
+		$(SELECTOR.CONTENTS_AREA).appendChild(tab);
 	}
 
 	clearContentsArea() {
