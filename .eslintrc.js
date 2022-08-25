8133caae0921f532fe94cb112fc58d2a1cdc648a
Chris.c <cjs120dd@gmail.com>
2022-08-25 15:08:45 +0900

    chore:파싱 문제 해결

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,24 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		node: true,
-	},
-	extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
-	plugins: ['prettier', 'mocha'],
-	rules: {
-		'import/prefer-default-export': 'off',
-		'import/extensions': ['off'],
-		'class-methods-use-this': 'off',
-		'no-alert': 'off',
-		'no-undef': 'off',
-		'array-callback-return': 'off',
-	},
+    parser: "@babel/eslint-parser",
+    parserOptions: {
+        parser: '@babel/eslint-parser',
+        requireConfigFile: false,
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+    extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
+    plugins: ['prettier', 'mocha', "@babel"],
+    rules: {
+        'import/prefer-default-export': 'off',
+        'import/extensions': ['off'],
+        'class-methods-use-this': 'off',
+        'no-alert': 'off',
+        'no-undef': 'off',
+        'array-callback-return': 'off',
+    },
 };
